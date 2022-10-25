243c765b3635d325ba23ae8ea6419e0ffc8b2ffd
Recep Terzi <recepterzi-67@hotmail.com>
2022-10-25 21:51:29 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import { Routes, Route } from "react-router-dom";
-import ExtraNavbar from "./components/ExtraNavbar/ExtraNavbar";
+import Contact from "./components/Contact/Contact";
 import Food from "./components/Food/Food";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
-import Navbar from "./components/Navbar/Navbar";
 import Register from "./components/Register/Register";
 
 function App() {
@@ -14,6 +13,7 @@ function App() {
         <Route path="/login" element={<Login />}></Route>
         <Route path="/register" element={<Register />}></Route>
         <Route path="/food" element={<Food />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
       </Routes>
     </>
   );
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,48 @@
+import React from "react";
+import ExtraNavbar from "../ExtraNavbar/ExtraNavbar";
+import Navbar from "../Navbar/Navbar";
+import contact from "../../assets/contact.png";
+import "./Contact.Module.css";
+const Contact = () => {
+  return (
+    <>
+      <ExtraNavbar />
+      <Navbar />
+      <div className="contact-container">
+        <div className="row">
+          <div className="col-md-6">
+            <img src={contact} alt="" />
+          </div>
+          <div className="col-md-6">
+            <div className="contact-title">
+              Bize ulaşın, uygun programı size sunalım.
+            </div>
+            <div className="contact-form">
+              <form>
+                <div className="username-form">
+                  <label>Adınız :</label>
+                  <input type="text" />
+                </div>
+                <div className="email-form">
+                  <label>Email Adresiniz:</label>
+
+                  <input type="text" />
+                </div>
+                <div className="message-form">
+                  <label>Mesajınız:</label>
+
+                  <textarea name="" id="" cols="20" rows="3"></textarea>
+                </div>
+                <div className="contact-button">
+                  <button>Gönder</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
